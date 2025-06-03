69527d2e583e5c255d36e3f83c9568b815ce4e32
Saad Umair <saad.ansari0106@gmail.com>
2025-06-03 21:25:14 +0530

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 import "./globals.css"
 
@@ -31,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>{children}</Providers>
+        <GoogleAnalytics gaId="G-6VSZ6G84BC"/>
       </body>
     </html>
   )
